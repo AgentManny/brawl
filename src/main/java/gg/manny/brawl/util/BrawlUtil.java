d4c952b65bbbc449b8ab5aa7a805104e9f2f60a7
AgentManny <itsreallymanny@gmail.com>
2019-02-21 16:30:46 +0000

    Added some utilities (DurationFormatted from IHCF)

--- src/main/java/gg/manny/brawl/util/BrawlUtil.java
+++ src/main/java/gg/manny/brawl/util/BrawlUtil.java
@@ -6,10 +6,13 @@ import gg.manny.brawl.Brawl;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandMap;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import javax.xml.stream.Location;
 import java.lang.reflect.Field;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
@@ -42,14 +45,36 @@ public class BrawlUtil {
         return item1.getType() == item2.getType() && (item1.hasItemMeta() && item1.getItemMeta().hasDisplayName() && item2.hasItemMeta() && item2.getItemMeta().hasDisplayName() && item1.getItemMeta().getDisplayName().equalsIgnoreCase(item2.getItemMeta().getDisplayName()));
     }
 
-    public static List<Entity> getNearbyPlayers(Player player, int radius) {
-        return player.getNearbyEntities(radius, radius, radius).stream().filter(entity -> entity instanceof Player).collect(Collectors.toList());
+
+    public static List<Player> getNearbyPlayers(Player player, double radius) {
+        return player.getNearbyEntities(radius, radius, radius).stream().filter(entity -> entity instanceof Player).map(entity -> ((Player)entity)).collect(Collectors.toList());
     }
 
     public static boolean has(JsonObject jsonObject, String key) {
         return jsonObject != null && !jsonObject.isJsonNull() && jsonObject.has(key) && jsonObject.get(key) != null && !jsonObject.isJsonNull();
     }
 
+    public static boolean isJson(String test) {
+        try {
+            new JSONObject(test);
+        } catch (JSONException ex) {
+            try {
+                new JSONArray(test);
+            } catch (JSONException ex1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isGeneric() {
+        try {
+            Class.forName("gg.manny.spigot.GenericSpigot");
+        } catch (ClassNotFoundException ignored) {
+            return false;
+        }
+        return true;
+    }
     public static void registerCommand(Command command) {
         Field bukkitCommandMap = null;
         try {
