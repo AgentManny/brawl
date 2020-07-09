7e76ff7a52b979118e14e718f0b7552ca1b1ed20
AgentManny <itsreallymanny@gmail.com>
2020-07-10 00:00:03 +0100

    All stompers should inflict reduced fall damage

--- src/main/java/rip/thecraft/brawl/ability/abilities/classic/Stomper.java
+++ src/main/java/rip/thecraft/brawl/ability/abilities/classic/Stomper.java
@@ -73,7 +73,7 @@ public class Stomper extends Ability implements Listener {
 
             ParticleEffect.EXPLOSION_HUGE.display(0, 0, 0, 0, 1, player.getLocation(), EFFECT_DISTANCE);
             player.playSound(player.getLocation(), Sound.ANVIL_LAND, 1.0F, 0.0F);
-            player.setFallDistance(0);
+            player.setFallDistance((float) (player.getFallDistance() / damageReduction)); // Fall damage should still apply to stompers but be reduced
             addCooldown(player); // Reset the cooldown
         }
     }
